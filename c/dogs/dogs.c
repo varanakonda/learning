5dcbe41d990c9ee616d1527c6d870e90f93c673f
Eugene Zalivadnyi <zalik132@gmail.com>
2025-07-30 22:58:48 +0200

    init C

---
+++ c/dogs/dogs.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(void) {
+    int dogs;
+    printf("Enter number of dogs you have: \n");
+    scanf("%d", &dogs);
+    printf("So, you have %d dogs!\n", dogs);
+    return 0;
+}
